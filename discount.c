a387ad6e6a5022078ffdb7d8ccbbf467525e7162
FricoSimon <simonfriko@gmail.com>
2023-08-28 13:56:17 +0700

    create discount.c file

---
+++ discount.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+float discount (float price, int percent);
+
+int main (void) {
+    float basePrice = get_float("what is the base price? ");
+    int percentage = get_int("what is the discount percentage? ");
+    float finalPrice = discount(basePrice, percentage);
+    printf("the final price is %.2f\n", finalPrice);
+}
+
+float discount (float price, int percent) {
+    return price * (100 - percent) / 100;
+}
