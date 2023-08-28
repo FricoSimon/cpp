da2b6f0f1e657dae0c58ba45d0a0326d0db8e63e
FricoSimon <simonfriko@gmail.com>
2023-08-28 13:04:43 +0700

    create agree.c file

---
+++ agree.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// gcc -o agree agree.c -lcs50
+
+int main (void) {
+    char c = get_char("Do you agree? ");
+
+    if(c == 'y' || c == 'Y') {
+        printf("Agreed.\n");
+    } else if (c == 'n' || c == 'N') {
+        printf("Not agreed.\n");
+    } else {
+        printf("Invalid option.\n");
+    }
+}
