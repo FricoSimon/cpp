0e06506a9b1bffd934be33f3f7a1464fb692ae73
FricoSimon <simonfriko@gmail.com>
2023-08-28 10:53:43 +0700

    remove makefile and run the command directly

--- calculator.c
+++ calculator.c
@@ -1,11 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// gcc -o calculator calculator.c -lcs50
+
 int main (void) {
     int x =  get_int("x: ");
     int y = get_int("y: ");
 
     int z = x + y;
-    
+
     printf("%i\n", z);
 }
--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// gcc -0 calculator calculator.c -lcs50
+
 int main (void) {
     string name = get_string("what is your name? ");
     printf("hello, %s\n", name);
